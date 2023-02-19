3e6faf68d324e7a9c273bda3ef6c1054574347ee
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-02-19 10:06:43 -0500

    finished

--- knights.cpp
+++ knights.cpp
@@ -2,11 +2,6 @@
 #include <fstream>
 #include <string>
 #include <unistd.h>
-#include "map"
-
-std::string me = "N";
-std::string path = ".";
-std::string visited = "v";
 
 void load(std::string filename, std::string maze[5][5])
 {
@@ -41,65 +36,68 @@ void print(std::string maze[5][5])
 	std::cout << "\n";
 }
 
-void print(int solution[5][5])
+void print_solution(std::string solution[5][5])
 {
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 5; j++)
 		{
-			if (solution[i][j] < 10 && solution[i][j] > 0)
+			if (std::stoi(solution[i][j]) < 10)
 			{
 				std::cout << " ";
 			}
-			std::cout << std::to_string(solution[i][j]) + ":";
+			std::cout << solution[i][j] + ":";
 		}
 		std::cout << "\n";
 	}
 }
 
-void solve(std::string maze[5][5], int row, int col, int i, bool solved, int s[5][5])
+void solve(std::string maze[5][5], int row, int col, int i, bool &solved)
 {
-	if (maze[row][col] == me || maze[row][col] == visited)
-	{
-		return;
-	}
-
-	if (i == 25) // no empty spaces left
+	if (i > 25) // no empty spaces left
 	{
 		solved = true;
+		print_solution(maze);
 		return;
 	}
 
-	maze[row][col] = me;
-	s[row][col] = i;
-	i++;
-	usleep(150000);
-	print(maze);
-
-	if (!solved && row + 2 < 5 && col + 1 < 5) // doesnt go out of bounds
-		solve(maze, row + 2, col + 1, i, solved, s);
-
-	if (!solved && row + 2 < 5 && col - 1 >= 0)
-		solve(maze, row + 2, col - 1, i, solved, s);
-
-	if (!solved && row + 1 < 5 && col + 2 < 5)
-		solve(maze, row + 1, col + 2, i, solved, s);
-
-	if (!solved && row + 1 < 5 && col - 2 >= 0)
-		solve(maze, row + 1, col - 2, i, solved, s);
-
-	if (!solved && row - 1 >= 0 && col - 2 >= 0)
-		solve(maze, row - 1, col - 2, i, solved, s);
-
-	if (!solved && row - 1 >= 0 && col + 2 < 5)
-		solve(maze, row - 1, col + 2, i, solved, s);
-
-	if (!solved && row - 2 >= 0 && col - 1 >= 0)
-		solve(maze, row - 2, col - 1, i, solved, s);
-
-	if (!solved && row - 2 >= 0 && col + 1 < 5)
-		solve(maze, row - 2, col + 1, i, solved, s);
+	if (maze[row][col] != ".")
+	{
+		return;
+	}
 
 	if (!solved)
-		maze[row][col] = visited;
+	{
+		maze[row][col] = std::to_string(i);
+	}
+
+	// usleep(150000);
+	// print(maze);
+
+	if (!solved && row + 2 < 5 && col + 1 < 5) // doesnt go out of bounds
+		solve(maze, row + 2, col + 1, i + 1, solved);
+
+	if (!solved && row + 2 < 5 && col - 1 >= 0)
+		solve(maze, row + 2, col - 1, i + 1, solved);
+
+	if (!solved && row + 1 < 5 && col + 2 < 5)
+		solve(maze, row + 1, col + 2, i + 1, solved);
+
+	if (!solved && row + 1 < 5 && col - 2 >= 0)
+		solve(maze, row + 1, col - 2, i + 1, solved);
+
+	if (!solved && row - 1 >= 0 && col - 2 >= 0)
+		solve(maze, row - 1, col - 2, i + 1, solved);
+
+	if (!solved && row - 1 >= 0 && col + 2 < 5)
+		solve(maze, row - 1, col + 2, i + 1, solved);
+
+	if (!solved && row - 2 >= 0 && col - 1 >= 0)
+		solve(maze, row - 2, col - 1, i + 1, solved);
+
+	if (!solved && row - 2 >= 0 && col + 1 < 5)
+		solve(maze, row - 2, col + 1, i + 1, solved);
+
+	if (!solved) // visited already
+		maze[row][col] = ".";
 }
--- main.cpp
+++ main.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <unistd.h>
 #include "knights.cpp"
-#include "map"
 
 int main()
 {
 	std::string maze[5][5];
-	int solution[5][5];
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			solution[5][5] = 0;
-		}
-	}
-
 	load("map.txt", maze);
 	bool solved = false;
 
-	solve(maze, 0, 3, 1, solved, solution);
-	print(solution);
+	solve(maze, 0, 0, 1, solved);
 
 	return 0;
 }
